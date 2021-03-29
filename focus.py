f49b53376a2d3f69def128bc026544debef8b918
Andy Grzywacz <agrzywacz3@gatech.edu>
2021-03-29 14:19:10 -0600

    stubbing out files

---
+++ focus.py
@@ -0,0 +1,28 @@
+# Interacts with the Focus db file atoring and retrieving workspaces and foci
+def setupFocusDb():
+
+    pass
+
+def getFocusDb():
+    pass
+
+def addWorkspace(name, path=None):
+    pass
+
+def removeWorkspace(name):
+    pass
+
+def setCurrentWorkspace(name):
+    pass
+
+def getCurrentWorkspace():
+    pass
+
+def getWorkspace(name):
+    pass
+
+def addFocus(name, ws=None):
+    pass
+
+def removeFocus(name):
+    pass
